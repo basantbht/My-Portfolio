a61fc38550766f229564b5af82cb83a78c9da510
basantbht <bhattbasant012@gmail.com>
2025-11-21 20:38:37 +0545

    changed message of contact page

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,15 +1,17 @@
 "use client";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FaEnvelope, FaPhone } from "react-icons/fa";
-import {motion} from "framer-motion"
+import { motion } from "framer-motion";
 
 const Contact = () => {
   const [result, setResult] = useState("");
+  const [isSuccess, setIsSuccess] = useState<boolean>(false);
+  const [sendingMessage, setSendingMessage] = useState("");
 
   const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    setResult("Sending...");
+    setSendingMessage("Sending...");
     const form = event.currentTarget;
     const formData = new FormData(form);
 
@@ -22,33 +24,52 @@ const Contact = () => {
 
     const data = await response.json();
     if (data.success) {
-      setResult("Message sent successfully ✔");
+      setSendingMessage("");
+      setIsSuccess(true);
+      setResult("Message sent successfully!");
       form.reset();
     } else {
+      setSendingMessage("");
+      setIsSuccess(false);
       setResult("Something went wrong. Try again.");
     }
   };
 
+  useEffect(() => {
+    if (!result) return;
+
+    const timer = setTimeout(() => {
+      setResult("");
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, [result]);
+
   return (
-    <motion.section 
-    initial={{opacity: 0}}
-    whileInView={{opacity: 1}}
-    transition={{duration: 1}}
-    id="contact" className="py-20 container max-w-6xl mx-auto px-4 scroll-my-5">
-      <motion.h1 
-      initial={{opacity: 0}}
-    whileInView={{opacity: 1}}
-    transition={{delay: 0.3, duration: 0.5}}
-      className="text-4xl font-bold text-center mb-12">Contact <span className="text-primary">Me</span></motion.h1>
+    <motion.section
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+      id="contact"
+      className="py-20 container max-w-6xl mx-auto px-4 scroll-my-5"
+    >
+      <motion.h1
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ delay: 0.3, duration: 0.5 }}
+        className="text-4xl font-bold text-center mb-12"
+      >
+        Contact <span className="text-primary">Me</span>
+      </motion.h1>
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
-
         {/* LEFT SIDE CONTACT INFO */}
-        <motion.div 
-        initial={{opacity: 0}}
-    whileInView={{opacity: 1}}
-    transition={{delay: 0.6, duration: 0.5}}
-        className="space-y-10">
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ delay: 0.6, duration: 0.5 }}
+          className="space-y-10"
+        >
           <div>
             <h2 className="text-2xl font-semibold mb-3">Get in Touch</h2>
             <p className="text-gray-600 dark:text-gray-400 md:w-3/4">
@@ -86,19 +107,20 @@ const Contact = () => {
         </motion.div>
 
         {/* RIGHT SIDE FORM */}
-        <motion.div 
-        initial={{opacity: 0}}
-    whileInView={{opacity: 1}}
-    transition={{delay: 0.6, duration: 0.5}}
-        className="bg-white dark:bg-gray-900 p-8 rounded-2xl shadow-lg border border-gray-200 dark:border-gray-700">
-          <form onSubmit={onSubmit} className="space-y-6">
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ delay: 0.6, duration: 0.5 }}
+          className="bg-white dark:bg-gray-900 p-8 rounded-2xl shadow-lg border border-gray-200 dark:border-gray-700"
+        >
+          <form onSubmit={onSubmit} className="space-y-3.5">
             {/* NAME */}
             <div>
               <label className="block text-sm font-medium mb-2">Name</label>
               <motion.input
-              initial={{opacity: 0, x: -50}}
-    whileInView={{opacity: 1, x: 0}}
-    transition={{delay: 1.1, duration: 0.6}}
+                initial={{ opacity: 0, x: -50 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{ delay: 0.8, duration: 0.5 }}
                 required
                 type="text"
                 name="name"
@@ -111,9 +133,9 @@ const Contact = () => {
             <div>
               <label className="block text-sm font-medium mb-2">Email</label>
               <motion.input
-              initial={{opacity: 0, x: 50}}
-    whileInView={{opacity: 1, x: 0}}
-    transition={{delay: 1.2, duration: 0.6}}
+                initial={{ opacity: 0, x: 50 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{ delay: 0.8, duration: 0.5 }}
                 required
                 type="email"
                 name="email"
@@ -126,9 +148,9 @@ const Contact = () => {
             <div>
               <label className="block text-sm font-medium mb-2">Message</label>
               <motion.textarea
-              initial={{opacity: 0, y: 100}}
-    whileInView={{opacity: 1, y: 0}}
-    transition={{delay: 1.3, duration: 0.6}}
+                initial={{ opacity: 0, y: 100 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ delay: 0.8, duration: 0.6 }}
                 required
                 rows={4}
                 name="message"
@@ -139,16 +161,24 @@ const Contact = () => {
 
             {/* BUTTON */}
             <motion.button
-            whileHover={{scale: 1.03}}
-            transition={{ duration: 0.3}}
+              whileHover={{ scale: 1.03 }}
+              transition={{ duration: 0.3 }}
               type="submit"
-              className="w-full bg-primary text-white py-3 rounded-lg font-semibold hover:bg-primary/90 transition shadow-md"
+              className="w-full bg-primary text-white py-3 rounded-lg font-semibold hover:bg-primary/90 transition shadow-md cursor-pointer"
             >
               Send Message
             </motion.button>
 
             {/* RESULT */}
-            <p className="text-center text-sm mt-2">{result}</p>
+            <p className="text-center text-sm mt-2">{sendingMessage}</p>
+
+            {isSuccess ? (
+              <p className="text-center text-sm mt-2 text-green-500">
+                {result}
+              </p>
+            ) : (
+              <p className="text-center text-sm mt-2 text-red-500">{result}</p>
+            )}
           </form>
         </motion.div>
       </div>
