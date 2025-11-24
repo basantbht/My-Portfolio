11fa18a8b01c2663a2dbbde86c884671043cf269
basantbht <bhattbasant012@gmail.com>
2025-11-24 10:01:58 +0545

    theme changed of resume button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -66,7 +66,7 @@ const Navbar = () => {
             <Link
               href="/basantCV.pdf"
               download
-              className="px-4 py-2 rounded-lg shadow hover:opacity-90 transition text-gray-800 bg-gray-400 dark:bg-[#212428]/80 dark:text-gray-300
+              className="px-4 py-2 rounded-lg shadow hover:opacity-90 transition text-gray-800 bg-gray-400 dark:bg-gray-700 dark:text-gray-300
 "
             >
               Resume
